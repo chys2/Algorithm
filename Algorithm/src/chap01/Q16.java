dd6f3160d7475c413148080804d1defaad9ac54f
DESKTOP-Q00AR0U\LENOVO <yeosepchoi@naver.com>
2022-10-17 15:20:32 +0900

    no message

--- Algorithm/src/chap01/Q16.java
+++ Algorithm/src/chap01/Q16.java
@@ -10,7 +10,7 @@ public class Q16 {
 				System.out.print(" ");
 			}
 			for(int j = 1; j <= num; j++) {
-				System.out.print(i);
+				System.out.print(i % 10);
 			}
 			System.out.println();
 		}		
@@ -19,6 +19,6 @@ public class Q16 {
 
 	public static void main(String[] args) {
 
-		npira(4);
+		npira(11);
 	}
 }
